f5d86910d9a0fb9f2c8ef81de91af245ef9d83cc
Arthur Monteiro <asilvamonteiro@icloud.com>
2026-05-27 15:40:11 -0300

    Atualização - Listando os clientes em um arquivo TEXTO para futuramente salvar em um Banco de Dados, e criando o método 'Deletar'

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,75 @@
+﻿using CadastroClientes.Models;
+using CadastroClientes.Models.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost("Salvar")]
+        public object Salvar([FromBody] Clientes cadastro)
+        {   
+            try
+            {
+                ClientesRepository clientes = new ClientesRepository();
+                clientes.Salvar(cadastro);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpPost("Alterar")]
+        public object Alterar([FromBody] Clientes cadastro)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpGet("Listar")]
+        public object Listar()
+        {
+            List<Clientes> listaCli = null;
+            try
+            {
+                ClientesRepository clientesRepo = new ClientesRepository();
+                listaCli = clientesRepo.Listar();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return listaCli;
+        }
+
+        [HttpDelete("Deletar")]
+        public object Deletar(int IdCliente)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Models/Repository/ClientesRepository.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace CadastroClientes.Models.Repository
+{
+    public class ClientesRepository
+    {
+        public void Salvar(Clientes clientes)
+        {
+            string clientesTexto = JsonConvert.SerializeObject(clientes) + "," + Environment.NewLine;
+            File.AppendAllText("C:\\Users\\famon\\Downloads\\Projettos\\CadastroClientes\\BancoDados\\bancodados.txt", clientesTexto);
+        }
+
+        public List<Clientes> Listar()
+        {
+            var clientes = File.ReadAllText("C:\\Users\\famon\\Downloads\\Projettos\\CadastroClientes\\BancoDados\\bancodados.txt");
+
+            List<Clientes> clientesLista = JsonConvert.DeserializeObject<List<Clientes>>("[" + clientes + "]");
+
+            return clientesLista.OrderByDescending(t => t.Nome).ToList();
+        }
+
+        public bool Deletar(string Documento)
+        {
+            var listaClientes = Listar();
+            var item = listaClientes.Where(t => t.Documento == Documento);
+        }
+    }
+}
